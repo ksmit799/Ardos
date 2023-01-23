e5a9f635fc7fd617e9b301c7895a80b7df387055
Kylie Smith <kyle.smithy2@gmail.com>
2023-01-23 21:59:14 +1000

    Added lib submodules

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+
+#include <yaml-cpp/yaml.h>
+
 int main() {
+  YAML::Node config = YAML::LoadFile("config.yaml");
+
+  if (config["lastLogin"]) {
+    std::cout << "Last logged in: " << config["lastLogin"].as<std::string>() << "\n";
+  }
+
   return 0;
 }
