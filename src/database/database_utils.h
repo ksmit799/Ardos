c7e1e4e861c5e1677e43434eccd4297d9f31a6b1
Kylie Smith <kylie.smithy01@gmail.com>
2023-03-31 16:38:43 +1000

    Object creation

--- src/database/database_utils.h
+++ src/database/database_utils.h
@@ -11,6 +11,21 @@ namespace Ardos {
 
 typedef std::map<const DCField *, std::vector<uint8_t>> FieldMap;
 
+/**
+ * Exception thrown by many database utility functions.
+ */
+class ConversionException : public std::exception {
+public:
+  explicit ConversionException(const char *msg) : _message(msg) {}
+
+  [[nodiscard]] inline const char *what() const override {
+    return _message.c_str();
+  }
+
+private:
+  std::string _message;
+};
+
 class DatabaseUtils {
 public:
   /**
@@ -39,8 +54,73 @@ public:
   static void FieldToBson(bsoncxx::builder::stream::single_context builder,
                           DCPacker &packer);
 
+  /**
+   * Converts a bson value to a number.
+   * Specifically for handling unsigned integers.
+   * @tparam T
+   * @param value
+   * @return
+   */
   template <typename T>
-  static T BsonToNumber(const bsoncxx::types::bson_value::view &value);
+  static T BsonToNumber(const bsoncxx::types::bson_value::view &value) {
+    // TODO: Can we prevent a double alloc here?
+    int64_t i;
+    double d;
+
+    bool isDouble = false;
+
+    // We've got three fundamental number types
+    if (value.type() == bsoncxx::type::k_int32) {
+      i = value.get_int32().value;
+    } else if (value.type() == bsoncxx::type::k_int64) {
+      i = value.get_int64().value;
+    } else if (value.type() == bsoncxx::type::k_double) {
+      d = value.get_double().value;
+      isDouble = true;
+    } else {
+      throw ConversionException("Non-numeric BSON type encountered");
+    }
+
+    if (std::numeric_limits<T>::is_integer && isDouble) {
+      // The bson source value is a double, and we're expecting an integer.
+      double intPart;
+      if (modf(d, &intPart) != 0.0) {
+        throw ConversionException("Non-integer double encountered");
+      }
+
+      if ((int64_t)d > std::numeric_limits<int64_t>::max() ||
+          (int64_t)d < std::numeric_limits<int64_t>::min()) {
+        throw ConversionException(
+            "Excessively large (or small) double encountered");
+      }
+
+      i = static_cast<int64_t>(d);
+    } else if (!std::numeric_limits<T>::is_integer && !isDouble) {
+      // Integer to double. Simple enough:
+      d = (double)i;
+    }
+
+    // Special case: uint64_t is stored as an int64_t. If we're doing that,
+    // just cast and return:
+    if (std::is_same<T, uint64_t>::value) {
+      return static_cast<uint64_t>(i);
+    }
+
+    // For floating types, we'll just cast and return:
+    if (!std::numeric_limits<T>::is_integer) {
+      return static_cast<T>(d);
+    }
+
+    // For everything else, cast if in range:
+    auto max = static_cast<int64_t>(std::numeric_limits<T>::max());
+    auto min = static_cast<int64_t>(std::numeric_limits<T>::min());
+
+    if (i > max || i < min) {
+      throw ConversionException("Integer is out of range");
+    }
+
+    return static_cast<T>(i);
+  }
 };
 
 } // namespace Ardos
