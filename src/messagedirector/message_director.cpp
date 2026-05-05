9bc32194bdec95f7f878a9e790a96a32c83a8f2d
Kylie Smith <kylie.smithy01@gmail.com>
2026-05-06 07:59:48 +1000

    Don't lint framebuffer erase

--- src/messagedirector/message_director.cpp
+++ src/messagedirector/message_director.cpp
@@ -117,6 +117,8 @@ MessageDirector::MessageDirector() {
             // Partial frame; wait for more bytes before retrying.
             break;
           }
+          // parse() can't return more than size:
+          // NOLINTNEXTLINE(bugprone-narrowing-conversions)
           _frameBuffer.erase(_frameBuffer.begin(),
                              _frameBuffer.begin() + processed);
         }
